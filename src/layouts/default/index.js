bd2a655029e1501136d9b7ddbb639ae23a740d9c
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-03 21:00:00 +0100

    cleanup

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -3,7 +3,6 @@ import Default from '@/layouts/default/Default.vue';
 export default {
   name: 'DefaultLayout',
   created() {
-    console.log('parent', this.$parent);
     this.$parent.$emit('update:layout', Default);
   },
   render() {
---
+++ src/views/home/Home.vue
@@ -0,0 +1,12 @@
+<template src="./Home.html"></template>
+<style scoped lang="sass" src="./Home.sass"></style>
+<script>
+import DefaultLayout from '@/layouts/default';
+
+export default {
+  name: 'Home',
+  components: {
+    DefaultLayout,
+  },
+};
+</script>

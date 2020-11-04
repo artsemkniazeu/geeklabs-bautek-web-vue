563e708df922f808b476c80834d95818e631fa9e
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-04 20:05:00 +0100

    Navbar added

---
+++ src/components/common/navbar/Navbar.vue
@@ -0,0 +1,16 @@
+<template src="./Navbar.html"></template>
+<style scoped lang="sass" src="./Navbar.sass"></style>
+<script>
+import content from '@/services/data/content-service';
+
+const { navbar } = content;
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      brand: navbar.brand,
+    };
+  },
+};
+</script>

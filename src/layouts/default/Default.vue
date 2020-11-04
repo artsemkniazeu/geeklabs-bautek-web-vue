08201f3b354dfcb2802729e6a8d430adc26cafc3
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-04 20:06:35 +0100

    Adding footer and navbar to default layout

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,7 +1,14 @@
 <template src="./Default.html"></template>
 <style scoped lang="sass" src="./Default.sass"></style>
 <script>
+import Navbar from '@/components/common/navbar/Navbar.vue';
+import Footer from '@/components/common/footer/Footer.vue';
+
 export default {
   name: 'Default',
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>

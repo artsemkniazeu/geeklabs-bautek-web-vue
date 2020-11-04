fcde30ba84b339eb5a7d9048ce1016807eab93f7
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-04 20:06:08 +0100

    Footer added

---
+++ src/components/common/footer/Footer.vue
@@ -0,0 +1,21 @@
+<template src="./Footer.html"></template>
+<style scoped lang="sass" src="./Footer.sass"></style>
+<script>
+import content from '@/services/data/content-service';
+import FooterContacts from '@/components/common/footer/footer-contacts/FooterContacts.vue';
+
+const { footer } = content;
+
+export default {
+  name: 'Footer',
+  components: {
+    FooterContacts,
+  },
+  data() {
+    return {
+      contacts: footer.contacts,
+      map: footer.map,
+    };
+  },
+};
+</script>
---
+++ src/components/common/footer/footer-contacts/FooterContacts.vue
@@ -0,0 +1,23 @@
+<template src="./FooterContacts.html"></template>
+<style scoped lang="sass" src="./FooterContacts.sass"></style>
+<script>
+import contactsSchema from '@/schemas/footer/contacts';
+
+export default {
+  name: 'FooterContacts',
+  props: {
+    contacts: {
+      type: Object,
+      validator(value) {
+        const errors = contactsSchema.validate(value);
+        const valid = errors.length === 0;
+        console.log(value);
+        if (!valid) {
+          console.error('FooterContacts prop contacts', errors);
+        }
+        return valid;
+      },
+    },
+  },
+};
+</script>

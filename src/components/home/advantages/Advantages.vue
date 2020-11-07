83af5e3d647be23a7c8a72279d1506c31ff1adfd
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-07 19:34:15 +0100

    Advantages added

---
+++ src/components/home/advantages/Advantages.vue
@@ -0,0 +1,18 @@
+<template src="./Advantages.html"></template>
+<style scoped lang="sass" src="./Advantages.sass"></style>
+<script>
+import content from '@/services/data/content-service';
+
+const { home } = content;
+const { advantages } = home;
+const { title, items } = advantages;
+export default {
+  name: 'Advantages',
+  data() {
+    return {
+      title,
+      items,
+    };
+  },
+};
+</script>

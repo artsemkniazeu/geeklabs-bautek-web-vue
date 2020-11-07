2c4bc1a62bcd9ce3dd832bbee0b6e7eb944f99ec
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-07 19:55:01 +0100

    Products view added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/home/Home.vue';
+import Products from '@/views/products/Products.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/products/:tag?',
+    name: 'Products',
+    component: Products,
   },
 ];
 
---
+++ src/views/products/Products.vue
@@ -0,0 +1,15 @@
+<template src="./Products.html"></template>
+<style scoped lang="sass" src="./Products.sass"></style>
+<script>
+import DefaultLayout from '@/layouts/default';
+
+import Goods from '@/components/goods/Goods.vue';
+
+export default {
+  name: 'Products',
+  components: {
+    DefaultLayout,
+    Goods,
+  },
+};
+</script>

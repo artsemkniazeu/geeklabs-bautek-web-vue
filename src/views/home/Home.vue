5838e2e252cfefb788d7094836975832834660c2
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-07 19:50:49 +0100

    Home assemble

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,11 +2,19 @@
 <style scoped lang="sass" src="./Home.sass"></style>
 <script>
 import DefaultLayout from '@/layouts/default';
+import Intro from '@/components/home/intro/Intro.vue';
+import Products from '@/components/home/products/Products.vue';
+import Advantages from '@/components/home/advantages/Advantages.vue';
+import Gallery from '@/components/home/gallery/Gallery.vue';
 
 export default {
   name: 'Home',
   components: {
     DefaultLayout,
+    Intro,
+    Products,
+    Advantages,
+    Gallery,
   },
 };
 </script>

676925357f69a4b414c7e125ce3caa90217658d9
artsem.kniazeu <artsem.kniazeu@altkom.software.pl>
2020-11-07 19:50:17 +0100

    Products added

---
+++ src/components/home/products/Products.vue
@@ -0,0 +1,27 @@
+<template src="./Products.html"></template>
+<style scoped lang="sass" src="./Products.sass"></style>
+<script>
+import content from '@/services/data/content-service';
+
+const { home } = content;
+const { products } = home;
+const { title, items } = products;
+export default {
+  name: 'Products',
+  data() {
+    return {
+      title,
+      items,
+      publicPath: process.env.BASE_URL,
+    };
+  },
+  methods: {
+    image(url) {
+      return `${this.publicPath}${url}`;
+    },
+    getUrl(tag) {
+      return `/products/${tag}`;
+    },
+  },
+};
+</script>
